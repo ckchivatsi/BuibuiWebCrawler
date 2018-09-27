21d459a0d6549bcfe2dd2ad5ec1531949aba5921
ckchivatsi <kelvinchivatsi@hotmail.com>
2018-09-27 11:59:22 +0300

    Parsing HTML

---
+++ link_finder.py
@@ -0,0 +1,17 @@
+from html.parser import HTMLParser
+from urllib import parse
+
+class LinkFinder(HTMLParser):
+
+    def __init__(self):
+        super().__init__()
+
+    def handle_starttag(self, tag, attrs):
+        print(tag)
+    
+    def error(self, message):
+        pass
+    
+finder = LinkFinder()
+finder.feed('<html><head><title>Document</title></head>'
+            '<body><h1>Hello World</h1></body></html>')
