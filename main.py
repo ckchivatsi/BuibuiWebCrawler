55cd7b427a641ed5c659a84d74e1421516c02de4
ckchivatsi <kelvinchivatsi@hotmail.com>
2018-09-27 17:21:34 +0300

    Creating jobs

--- main.py
+++ main.py
@@ -14,3 +14,16 @@ NUMBER_OF_THREADS = 4
 queue = Queue()
 Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
 
+#Each link in queue is a new job
+def create_jobs():
+    for link in file_to_set(QUEUE_FILE):
+        queue.put(link)
+    queue.join()
+    crawl()
+
+#Check for items in queue and crawl
+def crawl():
+    queued_links = file_to_set(QUEUE_FILE)
+    if len(queued_links) > 0:
+        print(str(len(queued_links)) + ' links in the queue.')
+        create_jobs()
