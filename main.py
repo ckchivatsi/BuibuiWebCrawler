6144f8bd50275a76e96e0ac343578cabf479e4f2
ckchivatsi <kelvinchivatsi@hotmail.com>
2018-09-27 17:34:29 +0300

    Running the program

--- main.py
+++ main.py
@@ -14,6 +14,20 @@ NUMBER_OF_THREADS = 4
 queue = Queue()
 Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
 
+#Create worker threads/spiders/buibui (die when main exits)
+def create_workers():
+    for _ in range(NUMBER_OF_THREADS):
+        t = threading.Thread(target=work())
+        t.daemon = True
+        t.start()
+
+#Do next job in queue
+def work():
+    while True:
+        url = queue.get()
+        Spider.crawl_page(threading.current_thread().name, url)
+        queue.task_done()
+
 #Each link in queue is a new job
 def create_jobs():
     for link in file_to_set(QUEUE_FILE):
@@ -27,3 +41,7 @@ def crawl():
     if len(queued_links) > 0:
         print(str(len(queued_links)) + ' links in the queue.')
         create_jobs()
+
+#Main Program
+create_workers()
+crawl()
