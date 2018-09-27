05b335ed93a39b616c46b30e2e977cd28f4eaf01
ckchivatsi <kelvinchivatsi@hotmail.com>
2018-09-27 17:10:19 +0300

    First Buibui

--- domain.py
+++ domain.py
@@ -1,15 +1,20 @@
 from urllib.parse import urlparse
 
-#Get domain name (example.com)
+#####-ISSUE-#####
+#The following favours country specific domains (example.co.ke)
+
+#Get domain name (example.co.ke)
 def get_domain_name(url):
     try:
         results = get_sub_domain_name(url).split('.')
         return results[-3] + '.' + results[-2] + '.' + results[-1]
+        #For single level domain suffix (example.com);
+        #return results[-2] + '.' + results[-1]
     except:
         return ''
         
 
-#Get sub domain name (sub.example.com)
+#Get sub domain name (sub.example.co.ke)
 def get_sub_domain_name(url):
     try:
         return urlparse(url).netloc
